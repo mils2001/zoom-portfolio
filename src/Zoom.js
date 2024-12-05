77912a68f3874c10d3c51269bbf12d3d7dac7d09
chris <Mileschris466@gmail.com>
2024-12-05 19:40:35 +0300

    backend

---
+++ src/Zoom.js
@@ -0,0 +1,14 @@
+import React, { Component,useState,useEffect } from 'react'
+
+export class Zoom extends Component {
+  render() {
+    return (
+      <div>
+        <img src='https://i.imgur.com/jFvVx5c.png' alt='portfolio-pic' className='pic'/>
+
+      </div>
+    )
+  }
+}
+
+export default Zoom
