d13a65102c76a5ca9f8593869f60f5070f03eb73
chris <Mileschris466@gmail.com>
2024-12-09 11:45:55 +0300

    updates on fetch

--- src/Zoom.js
+++ src/Zoom.js
@@ -1,6 +1,30 @@
 import React, {useState,useEffect} from 'react'
+import './Zoom.css'
+
+const API_URL = "http://localhost:3000/portfolio";
 
 function Zoom() {
+const [infoList,setInfoList] = useState([''])
+const [description,setDescription] = useState('');
+const [screenshot,setScreenshot] = useState('');
+const [isDetailsPopupVisible,setIsDetailsPopupVisible] = useState(false);
+const [alertMessage,setAlertMessage] = useState('');
+
+
+const fetchInfo= async() => {
+  try {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    setInfoList(data);
+    setDescription(data.description);
+    setScreenshot(data.screenshot);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
+useEffect(() => {
+  fetchInfo();
+}, []);
   return (
     <div className='container'>
       <div>
