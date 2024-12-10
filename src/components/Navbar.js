c11876c3ea30dbaa5aa2bd1c4d0fc6b33fa63f19
chris <Mileschris466@gmail.com>
2024-12-10 18:47:33 +0300

    navbar update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,48 @@
-import React from 'react'
-import './Navbar.css'
-import { Link } from "react-router-dom";
-
+import React, { useState } from 'react';
+import './Navbar.css';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
-    <div className='container'>
-        <nav className='navbar'>
-            <div>
-            <ul className='nav-links'>
-                <li>
-                    <Link to='./pages/Home.js'>Home</Link>
-                </li>
-                <li>
-                    <Link to='./pages/About.js' >About</Link>
-                </li>
-                <li>
-                    <Link to='./pages/Contact.js'>Contact</Link>
-                </li>
-                <li>
-                    <Link to='./pages/experiece.js'>Experience</Link>
-                </li>
-                <li>
-                    <Link to='./pages/work.js'>Work</Link>
-                </li>
-            </ul>
-            <div className='btn-container'>
-                <button className='btn' >RESUME</button>
-            </div>
-            </div>
-        </nav>
+    <div className="container">
+      <nav className="navbar">
+        <div className="logo">
+          <Link to="/">MyLogo</Link>
+        </div>
+        <ul className={`nav-links ${isMobileMenuOpen ? 'mobile-open' : ''}`}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/experience">Experience</Link>
+          </li>
+          <li>
+            <Link to="/work">Work</Link>
+          </li>
+        </ul>
+        <div className="btn-container">
+          <button className="btn">RESUME</button>
+        </div>
+        <div className="hamburger-menu" onClick={toggleMobileMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
