8d1f3bed61ba749ed9ba8b1c8da9e8f78ab4d731
chris <Mileschris466@gmail.com>
2024-12-12 09:47:36 +0300

    debugging

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import './App.css';
-import React, {BrowserRouter,Routes,Route} from "react-router-dom"
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Zoom from './Zoom';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-      
-    
-      <Navbar />
-      <Zoom />
-      <Routes>
-        <Route path="./components/pages/Home.js"  />
-        <Route path="./components/pages/About.js" element={<h1>About Us</h1>} />
-        <Route path="./components/pages/Contact.js" element={<h1>Contact Us</h1>} />
-      </Routes>
-      </BrowserRouter>
-      
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Zoom />} />
+          <Route path="/home" element={<h1>Home</h1>} />
+          <Route path="/about" element={<h1>About Us</h1>} />
+          <Route path="/contact" element={<h1>Contact Us</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
